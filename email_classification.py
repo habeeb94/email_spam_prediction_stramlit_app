ae7cb7733b38418dfdb4dddfa88147d30f2f08c8
habeeb94 <54321934+habeeb94@users.noreply.github.com>
2023-02-08 13:37:51 +0000

    Update email_classification.py

--- email_classification.py
+++ email_classification.py
@@ -6,6 +6,9 @@ import xgboost as xgb
 import streamlit as st
 import pandas as pd
 import numpy
+from collections import Counter
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
 
 ## Loadin the created model
 model = xgb.XGBRegressor()
@@ -13,3 +16,79 @@ model.load_model('xgb_model.json')
 
 ##Caching the model for faster loading
 @st.cache
+
+## data Cleaning............Creating Counters for striped email strings
+class EmailToWordsCount(BaseEstimator, TransformerMixin):
+    def __init__(self, strip_headers=True, to_lowercase=True, remove_punc=True, do_stem=True):
+        self.strip_headers = strip_headers
+        self.to_lowercase = to_lowercase
+        self.remove_punc = remove_punc
+        self.do_stem = do_stem
+        
+        # To perform stemming
+        self.stemmer = nltk.PorterStemmer()
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X, y=None):
+        X_word_counts = []
+        for email in X:
+            # text of the email
+            plain = email_to_text(email)
+            if plain is None:
+                plain = "nothing"
+            
+            if self.to_lowercase:
+                plain = plain.lower()
+            
+            if self.remove_punc:
+                plain = plain.replace(".", "")
+                plain = plain.replace(",", "")
+                plain = plain.replace("!", "")
+                plain = plain.replace("?", "")
+                plain = plain.replace(";", "")
+                
+            word_counts = Counter(plain.split())
+            if self.do_stem:
+                # Stem the word, and add their counts
+                stemmed_word_counts = Counter()
+                for word, count in word_counts.items():
+                    root_word = self.stemmer.stem(word)
+                    stemmed_word_counts[root_word] += count
+                word_counts = stemmed_word_counts
+            
+            X_word_counts.append(word_counts)
+        return np.array(X_word_counts)
+
+class WordCountVectorizer(BaseEstimator, TransformerMixin):
+    def __init__(self, vocabulary_size=1000):
+        self.vocabulary_size = vocabulary_size
+    # train on list of word counts and build vocabulary
+    def fit(self, X, y=None):
+        total_word_counts = Counter()
+        for word_count in X:
+            for word, count in word_count.items():
+                total_word_counts[word] += count
+                
+        # Build a vocabulary out of total most common
+        self.most_common = total_word_counts.most_common()[:self.vocabulary_size]
+        self.vocabulary_ = {word: i for i, (word, count) in enumerate(self.most_common)}
+    
+        return self
+    # Create the vector out of vocabulary
+    def transform(self, X, y=None):
+        X_new = np.zeros([X.shape[0], self.vocabulary_size + 1], dtype=int)
+        
+        # The vectors will contain additional column for counts of words
+        # not captured in vocabulary
+        for row, word_counts in enumerate(X):
+            for word, count in word_counts.items():
+                col = self.vocabulary_.get(word, self.vocabulary_size)
+                X_new[row, col] += count
+                
+        return X_new
+    
+   #Creating a Pipeline for Data processing
+email_to_countvector = Pipeline([
+    ("emailToWords", EmailToWordsCount()), 
+    ("wordCountVectorizer", WordCountVectorizer())
+])
